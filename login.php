c030986365427738b9c0275c0d62cd7e5b2b70e5
maryam <mgambo@academic.rrc.ca>
2023-03-20 22:54:55 -0500

    modified field validations for username and validated password from database while logging in. Created a new session to store logged in users status

--- login.php
+++ login.php
@@ -3,11 +3,65 @@
     
     Name: Maryam Ayemlo Gambo
     Date:March 20th, 2023
-    Description: This page allows a user with registerd account to login
+    Description: This page allows a user with registered account to login and have CRUD admin access
 
 ****************/
 
-require('connect.php');
+session_start();
+
+
+$emptyfieldError;
+$invalidUsername;
+$invalidPassword;
+$successMessage;
+
+if($_POST){
+	if ( empty($_POST['username']) || empty( $_POST['password'])) {
+		// Could not get the data that should have been sent.
+		$emptyfieldError = 'Fields cannot be empty. Please fill both the username and password fields!';
+	}
+	else{  
+	    require('connect.php');
+		//sanitize username field and store in a variable
+		$username = filter_input(INPUT_POST, 'username', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+
+		//select query
+	    $query = "SELECT ID,Password,Is_Admin FROM users WHERE Username = :username";
+
+	    // A PDO::Statement is prepared from the query.
+	    $statement = $db->prepare($query);
+	    $statement->bindValue(':username', $username);
+
+	    // Execution on the DB server is delayed until we execute().
+	    $statement->execute(); 
+	    $userDetails = $statement->fetch();
+	    print_r ($userDetails);
+
+	    if(!empty($userDetails)){
+	        if (password_verify($_POST['password'], $userDetails['Password'])){
+				// Verification success! User has logged-in!
+				// Create sessions, so we know the user is logged in.
+				$_SESSION['loggedin'] = TRUE;
+				$_SESSION['name'] = $_POST['username'];
+				$_SESSION['id'] = $userDetails['ID'];
+				$successMessage =  'Login successful. Welcome ' . $_SESSION['name'] . '!';
+				 header("	Location: pageAdministration.php");
+		       } 
+
+		    else {
+				// Incorrect password
+				$invalidPassword =  'Incorrect password!';
+		}
+	    }
+	    else {
+			// Incorrect username
+			$invalidUsername = "Incorrect username and/or password!";
+	    }
+
+
+		
+	}
+}
 ?>
 
 
@@ -20,17 +74,33 @@ require('connect.php');
 </head>
 
 <body>
+	<h1>Login</h1>
 	<form method="post" action="login.php">
 		<label for="username">Username</label><br>
         <input id="username" name="username" type="text"><br><br>
+       
         <label for="password">Password</label><br>
-        <input id="password" name="password" type="text"><br><br>
+        <input id="password" name="password" type="password" placeholder="password"><br><br>
 
+        <?php if(isset($invalidUsername)):?>
+        	<span class="error"><?= $invalidUsername?></span><br>
+        <?php endif ?>
 
+        <?php if(isset($invalidPassword)):?>
+        	<span class="error"><?= $invalidPassword?></span><br>
+        <?php endif ?>
 
+        <?php if(isset($emptyfieldError)):?>
+        	<span class="error"><?= $emptyfieldError?></span><br>
+        <?php endif ?>
 
-		<input type="submit" value="Login" id="login">	
+		<button type="submit" value="Login" id="login">	Login</button>
 	</form>
+	<?php if(isset($successMessage)):?>
+        	<h3><?= $successMessage ?></h3>
+    <?php endif ?>
+
+
 
 
 
