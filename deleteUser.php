d7012958a45e1e826d1d5a85f92819002195bfae
maryam <mgambo@academic.rrc.ca>
2023-03-21 21:36:30 -0500

    created delete functionality for users by getting the id for the GET Postglobal, sanitizing the id value and creating an sql delete query to delete the user roe from the users database

---
+++ deleteUser.php
@@ -0,0 +1,40 @@
+<?php
+
+/*******w******** 
+    
+    Name: Maryam Ayemlo Gambo
+    Date: March 21, 2023
+    Description: this page allows admin and logged in users to edit a movies poage(validates and sanitizes input fields) and updates it in the database.
+
+****************/
+require('connect.php');
+
+
+// Retrieve movie post to be deleted, if id GET parameter is in URL.
+if(isset($_GET['id'])){
+    // Sanitize the id. Like above but this time from INPUT_GET.
+    $id = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
+
+    //validate int
+    if(filter_input(INPUT_GET, 'id', FILTER_VALIDATE_INT) !== false){
+        // Build the parametrized delete SQL query using the filtered id.
+        $query = "DELETE FROM users WHERE id = :id LIMIT 1";
+        $statement = $db->prepare($query);
+
+
+        //  Bind values to the parameters
+        $statement->bindValue(':id', $id, PDO::PARAM_INT);
+                
+        // Execute the DELETE
+        $statement->execute();
+        
+        //redirect to admin page
+        header("Location:pageAdministration.php"); 
+    }
+    else{
+        header("Location:pageAdministration.php");
+    }
+}
+
+?>
+
